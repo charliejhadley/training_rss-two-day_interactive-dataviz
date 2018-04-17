30fbdbd7c81e125b800822320f61260402fe9d08
martinjhnhadley <martinjohnhadley@gmail.com>
2018-04-17 08:28:23 +0100

    bulk push

---
+++ Development/rss-day-two/THE-ORDER-OF-THINGS.R
@@ -0,0 +1,12 @@
+# Preamble
+# R_RStudio_and_Tidyverse
+# Get data into and ready for the tidyverse
+# htmlwidgets
+# leaflet
+# plotly
+# datatable
+# RMarkdown
+# Shiny_Basics
+# Shiny_layouts
+# reactivity_and_renderUI
+# Shiny_Advice
